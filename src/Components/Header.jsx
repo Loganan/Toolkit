17f6a3290282c7797d2e3e4aeb3cac55b5b94c09
Logan Walker <loganwalkernz@gmail.com>
2020-05-24 15:24:52 +1000

    Add header and footer components.

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PolymerIcon from '@material-ui/icons/Polymer';
+
+function Header() {
+    return (
+        <header>
+            <h1>
+                <PolymerIcon fontSize="large"/> Toolkit
+            </h1>
+        </header>
+    )
+}
+
+export default Header;
